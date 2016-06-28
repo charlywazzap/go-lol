7d77f07d91833e5d2fadfecfaf47bbe61c6b9a97
Juan Guzman <johnny@ooyala.com>
2016-06-28 16:50:00 -0500

    - cleaned up champion.go code

--- champion.go
+++ champion.go
@@ -7,37 +7,43 @@ type ChampionID int //for sure there will never even be more than a thousand cha
 
 // A Champion can be controlled by a Summoner in a Game
 type Champion struct {
-	AllyTips  []string `json:"allytips"`
-	Blurb     string   `json:"blurb"`
-	EnemyTips []string `json:"enemytips"`
-	ID        int      `json:"id"`
-	Image     ImageDto `json:"image"`
-	Info      struct {
-		Attack     int
-		Defense    int
-		Difficulty int
-		Magic      int
-	} `json:"info"`
-	Key     string `json:"key"`
-	Lore    string `json:"lore"`
-	Name    string `json:"name"`
-	Partype string `json:"partype"`
-	Passive struct {
-		Description          string   `json:"description"`
-		Image                ImageDto `json:"image"`
-		Name                 string   `json:"name"`
-		SanitizedDescription string   `json:"sanitizedDescription"`
-	} `json:"passive"`
-	Recommended []struct{} `json:"recommended"`
-	Skins       []struct {
-		ID   int    `json:"id"`
-		Name string `json:"name"`
-		Num  int    `json:"num"`
-	} `json:"skins"`
-	Spells []spellsData `json:"spells"`
-	Stats  champStats   `json:"stats"`
-	Tags   []string     `json:"tags"`
-	Title  string       `json:"title"`
+	AllyTips    []string     `json:"allytips"`
+	Blurb       string       `json:"blurb"`
+	EnemyTips   []string     `json:"enemytips"`
+	ID          int          `json:"id"`
+	Image       ImageDto     `json:"image"`
+	Info        info         `json:"info"`
+	Key         string       `json:"key"`
+	Lore        string       `json:"lore"`
+	Name        string       `json:"name"`
+	Partype     string       `json:"partype"`
+	Passive     passiveData  `json:"passive"`
+	Recommended []struct{}   `json:"recommended"`
+	Skins       []skinStats  `json:"skins"`
+	Spells      []spellsData `json:"spells"`
+	Stats       champStats   `json:"stats"`
+	Tags        []string     `json:"tags"`
+	Title       string       `json:"title"`
+}
+
+type info struct {
+	Attack     int
+	Defense    int
+	Difficulty int
+	Magic      int
+}
+
+type passiveData struct {
+	Description          string   `json:"description"`
+	Image                ImageDto `json:"image"`
+	Name                 string   `json:"name"`
+	SanitizedDescription string   `json:"sanitizedDescription"`
+}
+
+type skinStats struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+	Num  int    `json:"num"`
 }
 
 type champStats struct {
